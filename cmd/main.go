cfb5e232ad07826beb4337a619d25cd4bb89c355
Luzik-D <dima.luzik@gmail.com>
2023-09-15 17:52:19 +0300

    create and read config

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	".github.com/Luzik-D/BasicCRUD/internal/config"
+)
 
 func main() {
 	fmt.Println("Hello world")
+
+	// init logger
+	logger := log.Default()
+	logger.Println("Init logger")
+
+	// read config
+	cfgFile, err := config.Load()
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	fmt.Println(cfgFile)
+
+	// init storage (db connection)
+
+	// init http server
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module .github.com/Luzik-D/BasicCRUD
 
 go 1.20
+
+require gopkg.in/yaml.v2 v2.4.0 // indirect
